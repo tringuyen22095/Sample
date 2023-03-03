6514305f8485cbc3531f321c6ed5af740d5bd334
vna061tringuy <t.nguyenthanh@crif.com>
2023-03-03 09:50:26 +0700

    Init jenkin file only

--- backend/src/main/java/com/personal/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.personal;
-
-public class Constants {
-
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String AUTHORIZATION = "Authorization";
-
-}
--- backend/src/main/java/com/personal/util/ModelMapper.java
+++
@@ -1,9 +0,0 @@
-package com.personal.util;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public abstract class ModelMapper {
-
-}
