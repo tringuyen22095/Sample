d27a3681b8b6ae97907d42ad5c765558302f364e
vna061tringuy <t.nguyenthanh@crif.com>
2024-05-28 17:43:30 +0700

    UI

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import PaegOne from "page-one";
+
+export default function Home() {
+  return (
+    <>
+      
+      {PaegOne()}
+    </>
+  );
+}
